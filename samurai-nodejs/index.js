af8fcf3256631268f674a98c4af79c41bd28c42e
Lenilson Soares <lenilsonp40@gmail.com>
2023-02-22 14:44:53 -0300

    uptade

--- samurai-nodejs/index.js
+++ samurai-nodejs/index.js
@@ -1,24 +1,25 @@
+const { request, response } = require("express");
 const express = require("express");
 const server = express();
 
-server.get("/hello", (req, res) => {
-    const{ nome, idade } = req.query;
+server.use(express.json());
 
-    return res.json({ 
-        title: "Hello world",
-        message: `Olá ${nome} tudo bem!?`,
-        idade: idade
-     });
+let customers = [
+    { id: 1, name: "Dev Samurai", site: "http://devsamurai.com.br" },
+    { id: 2, name: "Google", site: "http://google.com" },
+    { id: 3, name:"UOL", site: "http://uol.com.br" }
+];
+
+server.get("/customers", (request, response) => {
+    return response.json(customers);
 });
 
-server.get("/hello/:nome", (req, res) => {
-    const nome = req.params.nome;
+server.get("/customers/:id", (request, Response) => {
+    const id = parseInt(request.params.id);
+    const customer = customers.find(item => item.id === id);
+    const status = customer ? 200 : 404;
 
-    return res.json({ 
-        title: "Hello world",
-        message: `Olá ${nome} tudo bem!?`,
-        
-     }); 
-})
+    return response.status(status).json(customer);
+});
 
 server.listen(3000);
