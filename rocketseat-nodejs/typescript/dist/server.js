ba775e62cc2f9649f821fb024d2688f6c9a4f8a6
Lenilson Soares <lenilsonp40@gmail.com>
2023-03-14 18:10:42 -0300

    up

--- rocketseat-nodejs/typescript/dist/server.js
+++ rocketseat-nodejs/typescript/dist/server.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" });
+    return response.json({ message: "funcionou!" });
 });
 app.listen(3333);
--- rocketseat-nodejs/typescript/src/server.ts
+++ rocketseat-nodejs/typescript/src/server.ts
@@ -1,9 +1,9 @@
-import express, { request, response } from 'express';
+import express, { request, response } from 'express'
 
 const app = express();
 
-app.get("/",(request, response) => {
-    return response.json({ message: "Tudo Certo!"})
+app.get("/",(request,response) => {
+    return response.json({ message: "funcionou!"})
 })
 
 app.listen(3333);
