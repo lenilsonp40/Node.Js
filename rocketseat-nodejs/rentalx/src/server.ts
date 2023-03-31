1b4b2aaab25cc4d3b0349f023a5d3d9600e1314b
Lenilson Soares <lenilsonp40@gmail.com>
2023-03-31 13:45:14 -0300

    up

--- rocketseat-nodejs/rentalx/src/server.ts
+++ rocketseat-nodejs/rentalx/src/server.ts
@@ -2,11 +2,17 @@ import express, { request, response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
     return response.json({ message: "Funcionando!"});
 });
 
-
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+     
+    return response.json({ name });
+});
 
 app.listen(3333, () => console.log("Server is running"));
 
