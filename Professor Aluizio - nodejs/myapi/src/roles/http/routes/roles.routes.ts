b426df804ddf3ad0dcf4a8f04ae86aef71de5ab7
Lenilson Soares <lenilsonp40@gmail.com>
2023-04-21 14:27:51 -0300

    roles

--- Professor Aluizio - nodejs/myapi/src/roles/http/routes/roles.routes.ts
+++ Professor Aluizio - nodejs/myapi/src/roles/http/routes/roles.routes.ts
@@ -1,22 +1,22 @@
 import { Router, response } from 'express';
-import { v4 as uuidv4 } from 'uuid';
 
 const rolesRouter = Router();
 
-const roles = [];
+const roles: Role[] = [];
 
 rolesRouter.post('/', (request, response) => {
-  const { name } = request.body
+  const { name } = request.body;
 
-  const role = {
-    id: uuidv4(),
+  const role = new Role();
+
+  Object.assign(role, {
     name,
     created_at: new Date(),
-  }
+  });
 
   roles.push(role);
 
   return response.status(201).json(role);
 });
 
-export { rolesRouter }
+export { rolesRouter };
