5107f0d52906cd2d23c7e7aaf4d7dd425de61b9a
Lenilson Soares <lenilsonp40@gmail.com>
2023-05-01 10:18:26 -0300

    Up

--- Professor Aluizio - nodejs/myapi/src/roles/http/routes/roles.routes.ts
+++ Professor Aluizio - nodejs/myapi/src/roles/http/routes/roles.routes.ts
@@ -1,16 +1,15 @@
 import { RolesRepository } from '@roles/repositories/RolesRepository';
-import { CreateRoleController } from '@roles/useCases/createRole/createRoleController';
+import { createRolesController } from '@roles/useCases/createRole';
 import { Router } from 'express';
 
 const rolesRouter = Router();
-const CreateRolesController = new CreateRoleController();
 const rolesRepository = new RolesRepository();
 
 rolesRouter.post('/', (request, response) => {
-  return CreateRolesController.handle(request, response)
+  return createRolesController.handle(request, response);
 });
 
-rolesRouter.get('/', (request, response) => {
+rolesRouter.get('/', (_request, response) => {
   const roles = rolesRepository.findAll();
 
   return response.json(roles);
--- Professor Aluizio - nodejs/myapi/src/roles/useCases/createRole/createRoleController.ts
+++ Professor Aluizio - nodejs/myapi/src/roles/useCases/createRole/createRoleController.ts
@@ -1,16 +1,15 @@
+import { Request, Response } from 'express';
 import { RolesRepository } from '@roles/repositories/RolesRepository';
 import { AppError } from '@shared/errors/AppErrors';
-import { Request, Response } from 'express';
+import { CreateRoleUseCase } from './CreateRoleUseCase';
 
 export class CreateRoleController {
+  constructor(private createRoleUseCase: CreateRoleUseCase) {}
+
   handle(request: Request, response: Response): Response {
     const { name } = request.body;
-    const rolesRepository = new RolesRepository();
-    const roleAlreadyExists = rolesRepository.findByName(name);
-    if (roleAlreadyExists) {
-      throw new AppError('Role already exists');
-    }
-    const role = rolesRepository.create({ name });
+    const role = this.createRoleUseCase.execute({ name });
+
     return response.status(201).json(role);
   }
 }
