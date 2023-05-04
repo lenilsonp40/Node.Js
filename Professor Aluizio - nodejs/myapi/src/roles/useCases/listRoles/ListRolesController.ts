8ffdcf513da0045a0e89f85c13807f6f711d2180
Lenilson Soares <lenilsonp40@gmail.com>
2023-05-04 17:24:23 -0300

    instance

--- Professor Aluizio - nodejs/myapi/src/roles/useCases/listRoles/ListRolesController.ts
+++ Professor Aluizio - nodejs/myapi/src/roles/useCases/listRoles/ListRolesController.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
-import { ListRolesUseCase  } from "./listRolesUseCase";
+import { Request, Response } from 'express';
+import { ListRolesUseCase } from './listRolesUseCase';
 
 export class ListRolesController {
   constructor(private listRolesUseCase: ListRolesUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const roles = this.listRolesUseCase.execute()
-    return response.json(roles)
+    const roles = this.listRolesUseCase.execute();
+    return response.json(roles);
   }
 }
--- Professor Aluizio - nodejs/myapi/src/roles/useCases/listRoles/index.ts
+++ Professor Aluizio - nodejs/myapi/src/roles/useCases/listRoles/index.ts
@@ -1,7 +1,7 @@
-import { RolesRepository } from "@roles/repositories/RolesRepository";
-import { ListRolesUseCase } from "./listRolesUseCase";
-import { ListRolesController } from "./ListRolesController";
+import { RolesRepository } from '@roles/repositories/RolesRepository';
+import { ListRolesUseCase } from './listRolesUseCase';
+import { ListRolesController } from './ListRolesController';
 
-const rolesRepository = new RolesRepository();
+const rolesRepository = RolesRepository.getInstance();
 const listRolesUseCase = new ListRolesUseCase(rolesRepository);
 export const listRolesController = new ListRolesController(listRolesUseCase);
